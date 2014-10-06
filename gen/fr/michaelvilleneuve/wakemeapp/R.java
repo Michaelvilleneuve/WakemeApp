d9e1aecb8914913a922b5ebc1215dd8fdb71b67e
VILLENEUVE <contact@michaelvilleneuve.fr>
2014-10-06 14:53:17 +0200

    Diverses amélioration
    menus et activités

--- gen/fr/michaelvilleneuve/wakemeapp/R.java
+++ gen/fr/michaelvilleneuve/wakemeapp/R.java
@@ -1091,6 +1091,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
+        public static final int action_search=0x7f050043;
         public static final int action_settings=0x7f05003e;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
@@ -1249,6 +1250,7 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
+        public static final int action_search=0x7f0a0015;
         public static final int action_settings=0x7f0a0010;
         public static final int app_name=0x7f0a000d;
         public static final int button_send=0x7f0a0012;
